3942e0c4609fff85e155b0b15ecb7ef9d4c6ab02
lomeli-noe <neolomeli@gmail.com>
2022-07-22 08:01:28 -0700

    Display shape info when user clicks on it

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Square : Shape
 {
+    private string m_name = "Cosmic Cube";
+    private float area;
+    private float cubeSide;
+
+    public GameObject textObj;
+    private TextMeshProUGUI nameText;
+
+    private void Start()
+    {
+        nameText = textObj.GetComponent<TextMeshProUGUI>();
+        cubeSide = gameObject.GetComponent<BoxCollider>().size.x;
+
+    }
+
     protected override void Greeting()
     {
-
-        Debug.Log("This is a square!");
+        textObj.gameObject.SetActive(true);
+        nameText.text = "name: " + cubeName + "\nArea: " + CubeArea();
     }
 
     private void OnMouseDown()
     {
         Greeting();
+    }
+
+    public string cubeName
+    {
+        get { return m_name; }
+    }
+
+    private string CubeArea()
+    {
+        area = 6 * Mathf.Sqrt(cubeSide);
+        return area.ToString();
     }
 }
